0a3dcca1e9887ce77977279b7f060394212a7930
LukasKodym <lukasz.kodym@gmail.com>
2019-03-25 18:47:09 +0100

    update

--- src/main/java/pl/lukas/springCourse/Starter.java
+++ src/main/java/pl/lukas/springCourse/Starter.java
@@ -2,7 +2,6 @@ package pl.lukas.springCourse;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import pl.lukas.springCourse.domain.DragonKillingKnight;
 import pl.lukas.springCourse.domain.Knighht;
 import pl.lukas.springCourse.domain.Quest;
 
@@ -18,7 +17,8 @@ public class Starter implements CommandLineRunner {
 
         System.out.println(lancelot);
 
-        DragonKillingKnight percival = new DragonKillingKnight("Percival", 31);
+        Quest killTheDragon = new Quest("Zabij smoka");
+        Knighht percival = new Knighht("Percival", 31, killTheDragon);
 
         System.out.println(percival);
 
