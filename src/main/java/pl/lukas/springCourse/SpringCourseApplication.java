3f6eb195f4060df11bce25593cd37f8fdc5e4623
LukasKodym <lukasz.kodym@gmail.com>
2019-03-26 17:27:49 +0100

    spring beans

--- src/main/java/pl/lukas/springCourse/SpringCourseApplication.java
+++ src/main/java/pl/lukas/springCourse/SpringCourseApplication.java
@@ -2,11 +2,15 @@ package pl.lukas.springCourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import pl.lukas.springCourse.domain.Castle;
 
 @SpringBootApplication
 public class SpringCourseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCourseApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(SpringCourseApplication.class, args);
+		Castle castle = (Castle) ctx.getBean("castle");
+		System.out.println(castle);
 	}
 }
