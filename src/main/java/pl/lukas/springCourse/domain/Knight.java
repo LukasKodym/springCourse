ea94ff901084a19262f9b764971d88e991fb3e53
LukasKodym <lukasz.kodym@gmail.com>
2019-03-26 18:02:59 +0100

    update

--- src/main/java/pl/lukas/springCourse/domain/Knight.java
+++ src/main/java/pl/lukas/springCourse/domain/Knight.java
@@ -34,6 +34,6 @@ public class Knight {
 
     @Override
     public String toString() {
-        return "Ryzerz o imieniu " + name + " (" +  age + /*"). Zadanie: " + quest +*/ ".";
+        return "Ryzerz o imieniu " + name + " (" +  age + ").";
     }
 }
