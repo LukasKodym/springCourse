cea15517a1267808af6c54b3dbe5590df101969a
LukasKodym <lukasz.kodym@gmail.com>
2019-03-27 17:27:13 +0100

    automatic component search

--- src/main/java/pl/lukas/springCourse/Starter.java
+++ src/main/java/pl/lukas/springCourse/Starter.java
@@ -3,12 +3,14 @@ package pl.lukas.springCourse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import pl.lukas.component.TestComponent;
 import pl.lukas.springCourse.domain.Castle;
-import pl.lukas.springCourse.domain.Knight;
-import pl.lukas.springCourse.domain.Quest;
 
 @Component
 public class Starter implements CommandLineRunner {
+
+    @Autowired
+    TestComponent test;
 
     @Autowired
     Castle castle;
