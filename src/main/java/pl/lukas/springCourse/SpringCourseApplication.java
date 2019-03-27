02bfc969bf75bb0db10e7114c85d17c076ead24c
LukasKodym <lukasz.kodym@gmail.com>
2019-03-27 17:31:44 +0100

    automatic component search update

--- src/main/java/pl/lukas/springCourse/SpringCourseApplication.java
+++ src/main/java/pl/lukas/springCourse/SpringCourseApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import pl.lukas.component.TestComponent;
 import pl.lukas.springCourse.domain.Castle;
+import pl.lukas.springCourse.domain.Knight;
+import pl.lukas.springCourse.domain.Quest;
 
 @SpringBootApplication
-@ComponentScan({"pl.lukas.springCourse", "pl.lukas.component"})
+// @ComponentScan({"pl.lukas.springCourse", "pl.lukas.component"})
+@ComponentScan(basePackageClasses = {Starter.class, Castle.class, Quest.class,
+        Knight.class, TestComponent.class} ) // this works similar as example above
 public class SpringCourseApplication {
 
     public static void main(String[] args) {
