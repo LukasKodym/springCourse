284ae410c41ae5fc55b88f3800423763db212923
LukasKodym <lukasz.kodym@gmail.com>
2019-03-28 17:32:41 +0100

    singleton example

---
+++ src/main/java/pl/lukas/springCourse/singletonExample/Address.java
@@ -0,0 +1,10 @@
+package pl.lukas.springCourse.singletonExample;
+
+public class Address {
+
+    private String name;
+
+    public Address(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/pl/lukas/springCourse/singletonExample/Person.java
@@ -0,0 +1,9 @@
+package pl.lukas.springCourse.singletonExample;
+
+public class Person {
+
+    private String name;
+    private int age;
+    private Address address;
+
+}
