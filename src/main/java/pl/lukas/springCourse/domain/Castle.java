a91b8796e9b99853e2aa5ebca1ba5d2781937766
LukasKodym <lukasz.kodym@gmail.com>
2019-04-01 08:52:19 +0200

    start commit

--- src/main/java/pl/lukas/springCourse/domain/Castle.java
+++ src/main/java/pl/lukas/springCourse/domain/Castle.java
@@ -1,7 +1,6 @@
 package pl.lukas.springCourse.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
