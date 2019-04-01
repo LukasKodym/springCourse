548ce2dfdd867fb58cb6f3c9e97d752a9e8f060e
LukasKodym <lukasz.kodym@gmail.com>
2019-04-01 10:27:20 +0200

    QuestRepository.class created

--- src/main/java/pl/lukas/springCourse/Starter.java
+++ src/main/java/pl/lukas/springCourse/Starter.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import pl.lukas.springCourse.domain.repository.KnightRepository;
+import pl.lukas.springCourse.domain.repository.QuestRepository;
 
 @Component()
 @Scope("singleton") // this setting is as a default, redundant to write this line
@@ -13,9 +14,12 @@ public class Starter implements CommandLineRunner {
     @Autowired
     KnightRepository knightRepository;
 
+    @Autowired
+    QuestRepository questRepository;
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println(knightRepository);
+        System.out.println(questRepository);
     }
 }
---
+++ src/main/java/pl/lukas/springCourse/domain/repository/QuestRepository.java
@@ -0,0 +1,34 @@
+package pl.lukas.springCourse.domain.repository;
+
+import org.springframework.stereotype.Repository;
+import pl.lukas.springCourse.domain.Quest;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class QuestRepository {
+    List<Quest> questList = new ArrayList<>();
+
+    public void createQuest(String description) {
+        questList.add(new Quest(description));
+    }
+
+    public List<Quest> getAll() {
+        return questList;
+    }
+
+    @PostConstruct
+    public void init(){
+        createQuest("Uratuj księżniczkę");
+        createQuest("Weź udział w turniju");
+    }
+
+    @Override
+    public String toString() {
+        return "QuestRepository{" +
+                "questList=" + questList +
+                '}';
+    }
+}
