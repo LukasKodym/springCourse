abfe2de5f3a716ea22d4c92239d9d4737e81a3ba
LukasKodym <lukasz.kodym@gmail.com>
2019-04-01 09:05:06 +0200

    creating list of Knights

--- src/main/java/pl/lukas/springCourse/domain/Tournament.java
+++ src/main/java/pl/lukas/springCourse/domain/Tournament.java
@@ -4,26 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 @Component
 public class Tournament {
 
     @Autowired
-    @Qualifier(value = "percival")
-    Knight knight;
+    List<Knight> knights;
 
     public Tournament() {
     }
 
     public void duel() {
-        knight.setAge(knight.getAge() + 1);
+//        knight.setAge(knight.getAge() + 1);
     }
 
-    public void setKnight(Knight knight) {
-        this.knight = knight;
+    public void setKnight(List<Knight> knights) {
+        this.knights = knights;
     }
 
     @Override
     public String toString() {
-        return "W turnieju bierze udział rycerz: " + knight;
+        return "W turnieju biorą udział rycerze: "
+                + knights.stream().map(Objects::toString).
+                collect(Collectors.joining(" , "));
     }
 }
