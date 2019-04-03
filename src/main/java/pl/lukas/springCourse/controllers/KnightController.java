1db4478d9e18b7bbb77d468d4aebd31069f70de8
LukasKodym <lukasz.kodym@gmail.com>
2019-04-03 08:12:48 +0200

    thymeleaf dependency added

--- src/main/java/pl/lukas/springCourse/controllers/KnightController.java
+++ src/main/java/pl/lukas/springCourse/controllers/KnightController.java
@@ -1,5 +1,26 @@
 package pl.lukas.springCourse.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.lukas.springCourse.domain.Knight;
+import pl.lukas.springCourse.services.KnightService;
 
+import java.util.List;
+
+
+@Controller
 public class KnightController {
+
+    @Autowired // in first step there was injected KnightRepository but better is to do via service
+    KnightService service;
+
+    @RequestMapping("/knights")
+    public String getKnights(Model model){
+        List<Knight> allKnights = service.getAllKnights();
+        model.addAttribute("knights",allKnights);
+        return "";
+    }
+
 }
---
+++ src/main/java/pl/lukas/springCourse/services/KnightService.java
@@ -0,0 +1,20 @@
+package pl.lukas.springCourse.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import pl.lukas.springCourse.domain.Knight;
+import pl.lukas.springCourse.domain.repository.KnightRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class KnightService {
+
+    @Autowired
+    KnightRepository knightRepository;
+
+    public List<Knight> getAllKnights() {
+        return new ArrayList<>(knightRepository.getAllKnights());
+    }
+}
