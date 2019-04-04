9d04c9604a3ead5fac04683bdc2c1ea879bed1ac
LukasKodym <lukasz.kodym@gmail.com>
2019-04-04 17:24:47 +0200

    class PlayerInformation to singleton

--- src/main/java/pl/lukas/springCourse/controllers/QuestController.java
+++ src/main/java/pl/lukas/springCourse/controllers/QuestController.java
@@ -3,6 +3,7 @@ package pl.lukas.springCourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import pl.lukas.springCourse.domain.Knight;
 import pl.lukas.springCourse.domain.Quest;
 import pl.lukas.springCourse.services.KnightService;
 import pl.lukas.springCourse.services.QuestService;
+
 import java.util.List;
 
 @Controller
@@ -22,18 +24,30 @@ public class QuestController {
     QuestService questService;
 
     @RequestMapping("/assignquest")
-    public String assignQuest(@RequestParam("knightId") Integer id, Model model){
+    public String assignQuest(@RequestParam("knightId") Integer id, Model model) {
         Knight knight = knightService.getKnight(id);
         List<Quest> notStartedQuests = questService.getAllNotStartedQuests();
-        model.addAttribute("knight" , knight);
-        model.addAttribute("notstartetquests" , notStartedQuests);
+        model.addAttribute("knight", knight);
+        model.addAttribute("notstartetquests", notStartedQuests);
         return "assignquest";
     }
+
     @RequestMapping(value = "/assignquest", method = RequestMethod.POST)
-    public String assignQuest(Knight knight){
+    public String assignQuest(Knight knight, BindingResult result) {
+        System.out.println(result);
         knightService.updateKnight(knight);
-        Quest quest = knight.getQuest();
-        questService.update(quest);
+//        Quest quest = knight.getQuest();
+//        questService.update(quest);
+        return "redirect:/knights";
+    }
+
+    @RequestMapping(value = "/checkquest")
+    public String checkQuest() {
+        List<Knight> allKnights = knightService.getAllKnights();
+        allKnights.forEach(knight -> {
+            knight.getQuest().isCompleted();
+        });
+        knightService.collectReward();
         return "redirect:/knights";
     }
 }
--- src/main/java/pl/lukas/springCourse/services/KnightService.java
+++ src/main/java/pl/lukas/springCourse/services/KnightService.java
@@ -3,10 +3,12 @@ package pl.lukas.springCourse.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pl.lukas.springCourse.domain.Knight;
+import pl.lukas.springCourse.domain.PlayerInformation;
 import pl.lukas.springCourse.domain.repository.KnightRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class KnightService {
@@ -32,5 +34,15 @@ public class KnightService {
 
     public void updateKnight(Knight knight) {
         knightRepository.updateKnight(knight.getId(), knight);
+    }
+
+    public int collectReward() {
+        int sum = knightRepository.getAllKnights().stream()
+                .filter(knight -> knight.getQuest().isCompleted())
+                .mapToInt(knight -> knight.getQuest().getReward())
+                .sum();
+        knightRepository.getAllKnights().stream()
+                .filter(knight -> knight.getQuest().isCompleted())
+                .forEach(knight -> knight.setQuest(null));
     }
 }
