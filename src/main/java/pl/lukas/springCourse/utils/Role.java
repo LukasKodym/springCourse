c27ba447afd1e084b5005977905989bde1351c12
LukasKodym <lukasz.kodym@gmail.com>
2019-04-05 14:38:16 +0200

    Role and RoleRepo classes added

--- src/main/java/pl/lukas/springCourse/utils/Role.java
+++ src/main/java/pl/lukas/springCourse/utils/Role.java
@@ -1,5 +1,25 @@
 package pl.lukas.springCourse.utils;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String userName;
+    private String userRole;
+
+    public Role() {
+    }
+
+    public Role(String userName, String userRole) {
+        this.userName = userName;
+        this.userRole = userRole;
+    }
 }
