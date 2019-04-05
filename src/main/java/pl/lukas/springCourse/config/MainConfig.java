a608b8c8753af36a212d88a295eac628824db61f
LukasKodym <lukasz.kodym@gmail.com>
2019-04-05 11:55:56 +0200

    security added, login and password for user, end lesson 7.1

--- src/main/java/pl/lukas/springCourse/config/MainConfig.java
+++ src/main/java/pl/lukas/springCourse/config/MainConfig.java
@@ -1,6 +1,8 @@
 package pl.lukas.springCourse.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import pl.lukas.springCourse.domain.repository.DBKnightRepository;
 import pl.lukas.springCourse.domain.repository.InMemoryRepository;
 import pl.lukas.springCourse.domain.repository.KnightRepository;
@@ -20,5 +22,17 @@ public class MainConfig {
     public KnightRepository createDBRepo(){
         KnightRepository repo = new DBKnightRepository();
         return repo;
+    }
+
+    @Autowired
+    public void securityUsers(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("user1")
+                .password("user1")
+                .roles("USER")
+                .and()
+                .withUser("user2")
+                .password("user2")
+                .roles("ADMIN");
     }
 }
